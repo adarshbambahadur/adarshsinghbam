4e0a4f99e47f55236fa9a12145afac3b6af9c1df
Adarsh Singh <adarshbambahadur@gmail.com>
2022-06-28 22:56:23 +0530

    updated work

--- script.js
+++ script.js
@@ -12,86 +12,87 @@ const blackLogo = document.querySelector('.black-logo')
 const contactForm = document.querySelector('.contact')
 const sendButton = document.querySelector('.submit-btn')
 const mainSection = document.querySelector('.main-section')
+const themeChanger = document.querySelector('#theme-changer');
 
 aboutMeBtn.addEventListener('click', () => {
-  aboutSection.classList.add('active')
-  overlay.classList.add('active')
-  navbar.classList.add('hidden')
-  navbar.classList.add('hidden')
-  socialIcons.classList.add('hidden')
-  disableScroll()
-})
+  aboutSection.classList.add('active');
+  overlay.classList.add('active');
+  navbar.classList.add('hidden');
+  navbar.classList.add('hidden');
+  socialIcons.classList.add('hidden');
+  disableScroll();
+});
 
 xIcon.addEventListener('click', () => {
-  aboutSection.classList.remove('active')
-  overlay.classList.remove('active')
-  navbar.classList.remove('hidden')
-  socialIcons.classList.remove('hidden')
-  enableScroll()
-})
+  aboutSection.classList.remove('active');
+  overlay.classList.remove('active');
+  navbar.classList.remove('hidden');
+  socialIcons.classList.remove('hidden');
+  enableScroll();
+});
 
 overlay.addEventListener('click', () => {
-  aboutSection.classList.remove('active')
-  overlay.classList.remove('active')
-  navbar.classList.remove('hidden')
-  socialIcons.classList.remove('hidden')
-  enableScroll()
-})
+  aboutSection.classList.remove('active');
+  overlay.classList.remove('active');
+  navbar.classList.remove('hidden');
+  socialIcons.classList.remove('hidden');
+  enableScroll();
+});
 
 // canvas.addEventListener('wheel', (e) => {
 //   window.scrollTo(0, (mainSection.clientHeight * e.deltaY) / Math.abs(e.deltaY))
 // })
 
 latestWorks.addEventListener('click', (e) => {
-  scrollTo(0, mainSection.clientHeight)
-})
+  scrollTo(0, mainSection.clientHeight);
+});
 
 downArrow.addEventListener('click', (e) => {
-  scrollTo(0, mainSection.clientHeight)
-})
+  scrollTo(0, mainSection.clientHeight);
+});
 
 latestWorksArrow.addEventListener('click', (e) => {
-  scrollTo(0, mainSection.clientHeight)
-})
+  scrollTo(0, mainSection.clientHeight);
+});
 
 blackLogo.addEventListener('click', (e) => {
-  scrollTo(0, 0)
-})
+  scrollTo(0, 0);
+});
 
 contactForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-  let myForm = contactForm
-  let formData = new FormData(myForm)
-  const buttonText = sendButton.innerHTML
+  e.preventDefault();
+  let myForm = contactForm;
+  let formData = new FormData(myForm);
+  const buttonText = sendButton.innerHTML;
   fetch('/', {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     body: new URLSearchParams(formData).toString(),
   })
     .then(() => {
-      sendButton.classList.add('sent')
-      sendButton.innerHTML = 'Message Sent'
-      console.log('Message Sent Successfully')
+      sendButton.classList.add('sent');
+      sendButton.innerHTML = 'Message Sent';
+      console.log('Message Sent Successfully');
     })
     .catch((error) => {
-      sendButton.classList.add('notSent')
-      sendButton.innerHTML = `Something went wrong!`
-      console.log(error)
+      sendButton.classList.add('notSent');
+      sendButton.innerHTML = `Something went wrong!`;
+      console.log(error);
     })
     .finally(() => {
       setTimeout(() => {
-        sendButton.classList.remove('sent')
-        sendButton.classList.remove('notSent')
-        sendButton.innerHTML = buttonText
-      }, 3000)
-    })
+        sendButton.classList.remove('sent');
+        sendButton.classList.remove('notSent');
+        sendButton.innerHTML = buttonText;
+      }, 3000);
+    });
 
   Array.from(contactForm.children).forEach((child, i) => {
     if (!(i % 2)) {
-      child.value = ''
+      child.value = '';
     }
-  })
-})
+  });
+});
 
 var swiper = new Swiper('.mySwiper', {
   loop: true,
@@ -115,65 +116,67 @@ var swiper = new Swiper('.mySwiper', {
     modifier: 1,
     slideShadows: false,
   },
-})
+});
 
 // left: 37, up: 38, right: 39, down: 40,
 // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-var keys = { 37: 1, 38: 1, 39: 1, 40: 1 }
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
 
 function preventDefault(e) {
-  e.preventDefault()
+  e.preventDefault();
 }
 
 function preventDefaultForScrollKeys(e) {
   if (keys[e.keyCode]) {
-    preventDefault(e)
-    return false
+    preventDefault(e);
+    return false;
   }
 }
 
 // modern Chrome requires { passive: false } when adding event
-var supportsPassive = false
+var supportsPassive = false;
 try {
   window.addEventListener(
     'test',
     null,
     Object.defineProperty({}, 'passive', {
       get: function () {
-        supportsPassive = true
+        supportsPassive = true;
       },
     })
-  )
+  );
 } catch (e) {}
 
-var wheelOpt = supportsPassive ? { passive: false } : false
+var wheelOpt = supportsPassive ? { passive: false } : false;
 var wheelEvent =
-  'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel'
+  'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
 
 // call this to Disable
 function disableScroll() {
-  window.addEventListener('DOMMouseScroll', preventDefault, false) // older FF
-  window.addEventListener(wheelEvent, preventDefault, wheelOpt) // modern desktop
-  window.addEventListener('touchmove', preventDefault, wheelOpt) // mobile
-  window.addEventListener('keydown', preventDefaultForScrollKeys, false)
-  console.log('Scroll Disabled')
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+  console.log('Scroll Disabled');
 }
 
 // call this to Enable
 function enableScroll() {
-  window.removeEventListener('DOMMouseScroll', preventDefault, false)
-  window.removeEventListener(wheelEvent, preventDefault, wheelOpt)
-  window.removeEventListener('touchmove', preventDefault, wheelOpt)
-  window.removeEventListener('keydown', preventDefaultForScrollKeys, false)
-  console.log('Scroll Enabled')
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+  console.log('Scroll Enabled');
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const observer = new MutationObserver((mutations) => {
-    
-  })
+  const observer = new MutationObserver((mutations) => {});
 
   observer.observe(document.body, {
     childList: true,
-  })
-})
+  });
+});
+
+themeChanger.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+});
